619eed5d9f352feed4565b03fe9d8a55346e9b41
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-23 07:24:47 +0100

    appsSection > apps order

--- src/data/app-items.js
+++ src/data/app-items.js
@@ -2,12 +2,45 @@
 const logoPath = '/assets/logos/'
 export const appItems = [
   {
-    name: 'reservia',
+    name: 'candychase',
     deployed: true,
-    logo: logoPath + 'reservia_logo.png',
-    deployLink: 'https://githubagneska.github.io/HAgnesGenay_2_19112020/',
-    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_2_19112020',
-    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_2_19112020/blob/master/README.md'
+    logo: logoPath + 'candychase_logo.png',
+    deployLink: 'https://candychase2020-ionic5.herokuapp.com/',
+    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_7_19112020',
+    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_7_19112020/blob/master/README.md'
+  },
+  {
+    name: 'hrnet',
+    deployed: false,
+    logo: logoPath + 'hrnet_logo.png',
+    logo2: 'hag_logo_2020_small.png',
+    deployLink: '',
+    repoLink: 'https://github.com/GitHubAgneska/HRNET-app',
+    readmeLink: 'https://github.com/GitHubAgneska/HRNET-app/blob/master/README.md'
+  },
+  {
+    name: 'sportsee',
+    deployed: false,
+    logo: logoPath + 'sportsee_logo_main.png',
+    deployLink: 'https://sportsee.herokuapp.com/',
+    repoLink: 'https://github.com/GitHubAgneska/SPORTSEE',
+    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_12_19112020/blob/master/README.md'
+  },
+  {
+    name: 'kasa',
+    deployed: true,
+    logo: logoPath + 'kasa_logo.png',
+    deployLink: 'https://mysterious-mesa-34251.herokuapp.com/#/home',
+    repoLink: 'https://github.com/GitHubAgneska/KASA',
+    readmeLink: ''
+  },
+  {
+    name: 'moneybank',
+    deployed: false,
+    logo: logoPath + 'argentBank_logo.png',
+    deployLink: '',
+    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_13_19112020',
+    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_13_19112020/blob/master/README.md'
   },
   {
     name: 'ohmyfood',
@@ -42,44 +75,11 @@ export const appItems = [
     readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_7_19112020/blob/master/README.md'
   },
   {
-    name: 'kasa',
+    name: 'reservia',
     deployed: true,
-    logo: logoPath + 'kasa_logo.png',
-    deployLink: 'https://mysterious-mesa-34251.herokuapp.com/#/home',
-    repoLink: 'https://github.com/GitHubAgneska/KASA',
-    readmeLink: ''
-  },
-  {
-    name: 'sportsee',
-    deployed: false,
-    logo: logoPath + 'sportsee_logo_main.png',
-    deployLink: 'https://sportsee.herokuapp.com/',
-    repoLink: 'https://github.com/GitHubAgneska/SPORTSEE',
-    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_12_19112020/blob/master/README.md'
-  },
-  {
-    name: 'moneybank',
-    deployed: false,
-    logo: logoPath + 'argentBank_logo.png',
-    deployLink: '',
-    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_13_19112020',
-    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_13_19112020/blob/master/README.md'
-  },
-  {
-    name: 'hrnet',
-    deployed: false,
-    logo: logoPath + 'hrnet_logo.png',
-    logo2: 'hag_logo_2020_small.png',
-    deployLink: '',
-    repoLink: 'https://github.com/GitHubAgneska/HRNET-app',
-    readmeLink: 'https://github.com/GitHubAgneska/HRNET-app/blob/master/README.md'
-  },
-  {
-    name: 'candychase',
-    deployed: true,
-    logo: logoPath + 'candychase_logo.png',
-    deployLink: 'https://candychase2020-ionic5.herokuapp.com/',
-    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_7_19112020',
-    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_7_19112020/blob/master/README.md'
+    logo: logoPath + 'reservia_logo.png',
+    deployLink: 'https://githubagneska.github.io/HAgnesGenay_2_19112020/',
+    repoLink: 'https://github.com/GitHubAgneska/HAgnesGenay_2_19112020',
+    readmeLink: 'https://github.com/GitHubAgneska/HAgnesGenay_2_19112020/blob/master/README.md'
   }
 ]
