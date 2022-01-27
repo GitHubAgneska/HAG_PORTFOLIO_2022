b648caff148ef857d05a2511ee81aaa693151aaf
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-27 09:22:41 +0100

    nav/burger / +route to home

--- src/components/elements/burger/Burger.jsx
+++ src/components/elements/burger/Burger.jsx
@@ -1,14 +1,15 @@
 import { StyledBurger } from './Buger_style'
-
+import { Link } from 'react-router-dom';
 
 const Burger = ({ open, setOpen, hostParent }) => {
 
+  const navigateHome = () => { }
   return (
 
     <StyledBurger hostParent={hostParent} open={open} onClick={setOpen}>
       {open
         ? <i className='fas fa-times' id='closeNavIcon' />
-        : <i className='fas fa-bars' id='burgerIcon' />}
+        : <Link to='/'><i className='fas fa-bars' id='burgerIcon' onClick={navigateHome}/></Link>}
     </StyledBurger>
   )
 }
