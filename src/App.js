ba6de47eac50ac27d9a1908abeb01f03b08eabad
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-02-05 11:58:48 +0100

    CODE SPLITTING > + lazy loading main sections pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import HomeContainer from './components/elements/HomeContainer/HomeContainer'
-import DevSection from './components/elements/Dev-section/DevSection'
-import WebdesignSection from './components/elements/Webdesign-section/WebdesignSection'
-import InfoSection from './components/elements/Info-section/InfoSection'
+import React, {Fragment, Suspense, lazy } from 'react'
+
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import HomeContainer from './components/elements/HomeContainer/HomeContainer'
 
 import { GlobalStyle } from './style/global_style'
-import Footer from './components/layout/Footer'
-  
+
+// import WebdesignSection from './components/elements/Webdesign-section/WebdesignSection'
+// import DevSection from './components/elements/Dev-section/DevSection'
+// import InfoSection from './components/elements/Info-section/InfoSection'
+const DevSection = lazy(() => import('./components/elements/Dev-section/DevSection') )
+const WebdesignSection = lazy(() => import('./components/elements/Webdesign-section/WebdesignSection'));
+const InfoSection = lazy(() => import('./components/elements/Info-section/InfoSection') )
+
 const App = () => {
   return (
 
@@ -15,14 +21,16 @@ const App = () => {
       <GlobalStyle />
       <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path='/' element={<HomeContainer />}/>
-            <Route path='/home' element={<HomeContainer />}/>
-            <Route path='/development' element={<DevSection />}/>
-            <Route path='/webdesign' element={<WebdesignSection />}/>
-            <Route path='/info' element={<InfoSection />}/>
-            <Route path="*"  element={<Navigate to="/" />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path='/' element={<HomeContainer />}/>
+              <Route path='/home' element={<HomeContainer />}/>
+              <Route path='/development' element={<DevSection />}/>
+              <Route path='/webdesign' element={<WebdesignSection />}/>
+              <Route path='/info' element={<InfoSection />}/>
+              <Route path="*"  element={<Navigate to="/" />} />
+            </Routes>
+            </Suspense>
       </BrowserRouter>
         <Footer />
     </div>
